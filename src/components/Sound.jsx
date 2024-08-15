7485c13424c39d285cc855b94ccd9b8f662685a5
Devansh <devanshjindal31@gmail.com>
2024-08-15 18:07:01 +0530

    error solved

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -19,14 +19,21 @@ function Sound() {
     };
 
     return (
-        <motion.button initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ delay: 1 }} className='text-forground rounded-full flex items-center justify-center custom-bg fixed top-4 right-2.5 xs:right-4 w-fit self-start z-50'>
+        <motion.button 
+            initial={{ scale: 0 }} 
+            animate={{ scale: 1 }} 
+            transition={{ delay: 1 }} 
+            className='text-forground rounded-full flex items-center justify-center custom-bg fixed top-4 right-2.5 xs:right-4 w-fit self-start z-50'
+        >
             <audio ref={audioRef} loop>
                 <source src={"/audio/back.mp3"} type='audio/mpeg' />
-                Your browser doesn't support the audio element.
+                Your browser doesn&apos;t support the audio element.
             </audio>
             <span onClick={toggleAudio} className='relative w-14 h-14 p-4 hover:text-accent'>
                 {
-                    isPlaying ? <Volume2 className='w-full h-auto' strokeWidth={1.5} /> : <VolumeX className='w-full h-auto' strokeWidth={1.5} />
+                    isPlaying ? 
+                    <Volume2 className='w-full h-auto' strokeWidth={1.5} /> : 
+                    <VolumeX className='w-full h-auto' strokeWidth={1.5} />
                 }
             </span>
         </motion.button>
